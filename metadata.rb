79777f8078d2433d35928669c5a07fc3a34c5de8
jubianchi <contact@jubianchi.fr>
2014-02-03 22:27:26 +0100

    Bump to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email       'contact@jubianchi.fr'
 license                'MIT'
 description            'Installs/Configures hhvm'
 long_description       IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version                '0.1.0'
+version                '0.2.0'
 
 depends                'apt'
 depends                'yum'
