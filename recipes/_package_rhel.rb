00e35630f1e9bfb0b8bd43f3e12eff13cc7d494d
integrii <integrii@gmail.com>
2014-11-19 20:50:35 -0800

    Added abilitiy to not install epel and hop5.in when deploying HHVM on Centos

--- recipes/_package_rhel.rb
+++ recipes/_package_rhel.rb
@@ -1,26 +1,30 @@
 include_recipe 'yum'
 
-case node['platform_version'].to_f
-  when 6.4, 6.5, 6.6, 6.7 
-    remote_file '/tmp/epel.rpm' do
-      source 'http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm'
-      action :create_if_missing
-    end
+if node['hhvm']['setup_centos_epel_repo'] == true
+  case node['platform_version'].to_f
+    when 6.4, 6.5, 6.6, 6.7 
+      remote_file '/tmp/epel.rpm' do
+        source 'http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm'
+        action :create_if_missing
+      end
 
-    package 'epel' do
-      source '/tmp/epel.rpm'
-      provider Chef::Provider::Package::Rpm
-      action :install
-    end
-  else
-    raise %W(Platform not supported: #{node['platform_family']} (#{node[:platform]}) #{node['platform_version']}).join(' ')
+      package 'epel' do
+        source '/tmp/epel.rpm'
+        provider Chef::Provider::Package::Rpm
+        action :install
+      end
+    else
+      raise %W(Platform not supported: #{node['platform_family']} (#{node[:platform]}) #{node['platform_version']}).join(' ')
+  end
 end
 
-yum_repository 'hhvm' do
-  baseurl 'http://www.hop5.in/yum/el6/'
-  gpgkey 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-HOP5'
-  action :create
-  notifies :run, 'bash[yum-clean-all]', :immediately
+if node['hhvm']['setup_centos_hhvm_repo'] == true
+  yum_repository 'hhvm' do
+    baseurl 'http://www.hop5.in/yum/el6/'
+    gpgkey 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-HOP5'
+    action :create
+    notifies :run, 'bash[yum-clean-all]', :immediately
+  end
 end
 
 bash 'yum-clean-all' do
